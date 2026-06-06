6b0a69a38627d504bc480fadc1f67b003ed2d71c
Jun Kye <itsfree@4a:cc:a6:17:91:8b.home>
2026-06-06 10:34:21 -0400

    Increase collapse icon visual weight

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mokja-alley-v184";
+const CACHE_NAME = "mokja-alley-v185";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
--- storage.js
+++ storage.js
@@ -1,5 +1,5 @@
 (function () {
-  const appAssetVersion = "v184";
+  const appAssetVersion = "v185";
   const keys = {
     initialized: "restaurant_initialized",
     lang: "restaurant_lang",
